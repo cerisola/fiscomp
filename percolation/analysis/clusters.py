454e6567c33493f80536650b60199b917fe4b1b0
Federico Cerisola <federico@cerisola.net>
2017-05-01 22:52:19 -0300

    [percolation] Make the calculation of cluster densities exclude the percolating cluster

--- percolation/analysis/clusters.py
+++ percolation/analysis/clusters.py
@@ -19,32 +19,18 @@ def percolating_cluster_strength(size, percolated, realizations, L):
 
 # % Multiple values (list) % #
 def cluster_densities_finite_list(count, percolated, realizations, L):
-    data = [cluster_densities(count[i], percolated[i], realizations[i], L[i]) for i in range(L.size)]
+    data = [cluster_densities_finite(count[i], percolated[i], realizations[i], L) for i in range(len(count))]
     return data
 
 
-def percolating_cluster_mass_list(size, percolated):
-    data = [percolating_cluster_mass(size[i], percolated[i]) for i in range(len(size))]
+def percolating_cluster_mass_list(size, percolated, realizations):
+    data = [percolating_cluster_mass(size[i], percolated[i], realizations[i]) for i in range(len(size))]
     return np.array(data)
 
 
-def percolating_cluster_density_list(size, percolated, L):
-    return percolating_cluster_mass_list(size, percolated)/(L*L)
+def percolating_cluster_density_list(size, percolated, realizations, L):
+    return percolating_cluster_mass_list(size, percolated, realizations)/(L*L)
 
 
-def percolating_cluster_strength_list(size, percolated, L):
-    return percolating_cluster_mass_list(size, percolated)/(L*L)
-
-
-# % Averaged values over many realizations % #
-def percolating_cluster_mass_average(size, percolated, p_percolation):
-    return percolating_cluster_mass_list(size, percolated) * p_percolation
-
-
-def percolating_cluster_density_average(size, percolated, p_percolation, L):
-    return percolating_cluster_mass_average(size, percolated, p_percolation)/(L*L)
-
-
-def percolating_cluster_strength_average(size, percolated, p_percolation, L):
-    return percolating_cluster_mass_average(size, percolated, p_percolation)/(L*L)
-
+def percolating_cluster_strength_list(size, percolated, realizations, L):
+    return percolating_cluster_mass_list(size, percolated, realizations)/(L*L)
